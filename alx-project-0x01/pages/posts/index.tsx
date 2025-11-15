72be65bd9c9fdee948ee74d678aa396121d06057
hany-7878 <htesfaye641@gmail.com>
2025-11-15 04:30:17 -0800

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
 
 const PostsPage: React.FC = () => {
   return (
     <div className="p-8">
+        <Header/>
       <h1 className="text-3xl font-bold mb-6">Posts</h1>
       <div className="grid gap-4">
         <PostCard title="Post 1" description="This is the first post" />
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layout/Header";
 import React from "react";
 
 const UsersPage: React.FC = () => {
   return (
     <div className="p-8">
+        <Header/>
       <h1 className="text-3xl font-bold mb-6">Users</h1>
       <ul className="list-disc list-inside">
         <li>User 1</li>
